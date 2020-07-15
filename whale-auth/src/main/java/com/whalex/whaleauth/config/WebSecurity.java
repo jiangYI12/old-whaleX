45e6580032d03a877d7b9db5e08172d6c7e751ff
🐋鲸鱼 <877798901@qq.com>
2020-07-15 22:10:48 +0800

    网关配置跨域

--- whale-auth/src/main/java/com/whalex/whaleauth/config/WebSecurity.java
+++ whale-auth/src/main/java/com/whalex/whaleauth/config/WebSecurity.java
@@ -56,4 +56,5 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
+
 }
---
+++ whale-gateway/src/main/java/com/whalex/whalegateway/config/WhalexCoreConfiguration.java
@@ -0,0 +1,45 @@
+package com.whalex.whalegateway.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
+import org.springframework.http.server.reactive.ServerHttpResponse;
+import org.springframework.web.cors.reactive.CorsUtils;
+import org.springframework.web.server.ServerWebExchange;
+import org.springframework.web.server.WebFilter;
+import org.springframework.web.server.WebFilterChain;
+import reactor.core.publisher.Mono;
+
+/**
+ * Description: 配置跨域
+ *
+ * @author: 🐋鲸鱼
+ * date: 2020/7/15 20:34
+ */
+@Configuration
+public class WhalexCoreConfiguration {
+    @Bean
+    public WebFilter corsFilter() {
+        return (ServerWebExchange ctx, WebFilterChain chain) -> {
+            ServerHttpRequest request = ctx.getRequest();
+            if (CorsUtils.isCorsRequest(request)) {
+                ServerHttpResponse response = ctx.getResponse();
+                HttpHeaders headers = response.getHeaders();
+                headers.set(HttpHeaders.ACCESS_CONTROL_ALLOW_ORIGIN, "*");
+                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_HEADERS, "*");
+                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_METHODS, "");
+                headers.add(HttpHeaders.ACCESS_CONTROL_ALLOW_CREDENTIALS, "true");
+                headers.add(HttpHeaders.ACCESS_CONTROL_EXPOSE_HEADERS, "*");
+                headers.add(HttpHeaders.ACCESS_CONTROL_MAX_AGE, "7200");
+                if (request.getMethod() == HttpMethod.OPTIONS) {
+                    response.setStatusCode(HttpStatus.OK);
+                    return Mono.empty();
+                }
+            }
+            return chain.filter(ctx);
+        };
+    }
+}

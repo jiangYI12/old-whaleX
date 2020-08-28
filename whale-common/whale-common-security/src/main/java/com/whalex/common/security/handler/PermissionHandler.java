ae535777df3a3ff8436b26fa5778cf1b4c86e1dc
江益 <877798901@qq.com>
2020-08-28 14:43:00 +0800

    接口权限注释，待实现

--- whale-common/whale-common-security/src/main/java/com/whalex/common/security/handler/PermissionHandler.java
+++ whale-common/whale-common-security/src/main/java/com/whalex/common/security/handler/PermissionHandler.java
@@ -1,59 +1,57 @@
 
 package com.whalex.common.security.handler;
 
-import com.zwx.core.cache.utils.CacheUtil;
-import com.zwx.core.secure.ZwxUser;
-import com.zwx.core.secure.constant.PermissionConstant;
-import com.zwx.core.secure.utils.AuthUtil;
-import com.zwx.core.tool.utils.Func;
-import com.zwx.core.tool.utils.StringPool;
-import com.zwx.core.tool.utils.WebUtil;
+
+import com.whalex.common.core.baseEntity.WhaleUsers;
+import com.whalex.common.core.util.AuthUtil;
+import com.whalex.common.tool.webUtil.WebUtils;
 import lombok.AllArgsConstructor;
-import org.springframework.jdbc.core.JdbcTemplate;
 
 import javax.servlet.http.HttpServletRequest;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 
-import static com.zwx.core.cache.constant.CacheConstant.SYS_CACHE;
 
 /**
  * 默认授权校验类
  *
  * @author livingdiy
  */
+@Deprecated
 @AllArgsConstructor
 public class PermissionHandler implements IPermissionHandler {
 
 	private static final String SCOPE_CACHE_CODE = "apiScope:code:";
 
-	private JdbcTemplate jdbcTemplate;
+//	private JdbcTemplate jdbcTemplate;
 
 	@Override
 	public boolean permissionAll() {
-		HttpServletRequest request = WebUtil.getRequest();
-		ZwxUser user = AuthUtil.getUser();
-		if (request == null || user == null) {
-			return false;
-		}
-		String uri = request.getRequestURI();
-		List<String> paths = permissionPath(user.getRoleId());
-		if (paths.size() == 0) {
-			return false;
-		}
-		return paths.stream().anyMatch(uri::contains);
+//		HttpServletRequest request = WebUtils.getRequest();
+//		WhaleUsers user = AuthUtil.getWhaleXUser();
+//		if (request == null || user == null) {
+//			return false;
+//		}
+//		String uri = request.getRequestURI();
+//		List<String> paths = permissionPath(user.getRoleId());
+//		if (paths.size() == 0) {
+//			return false;
+//		}
+//		return paths.stream().anyMatch(uri::contains);
+		return false;
 	}
 
 	@Override
 	public boolean hasPermission(String permission) {
-		HttpServletRequest request = WebUtil.getRequest();
-		ZwxUser user = AuthUtil.getUser();
-		if (request == null || user == null) {
-			return false;
-		}
-		List<String> codes = permissionCode(permission, user.getRoleId());
-		return codes.size() != 0;
+//		HttpServletRequest request = WebUtils.getRequest();
+//		WhaleUsers user = AuthUtil.getWhaleXUser();
+//		if (request == null || user == null) {
+//			return false;
+//		}
+//		List<String> codes = permissionCode(permission, user.getRoleId());
+//		return codes.size() != 0;
+		return false;
 	}
 
 	/**
@@ -62,15 +60,15 @@ public class PermissionHandler implements IPermissionHandler {
 	 * @param roleId 角色id
 	 * @return permissions
 	 */
-	private List<String> permissionPath(String roleId) {
-		List<String> permissions = CacheUtil.get(SYS_CACHE, SCOPE_CACHE_CODE, roleId, List.class);
-		if (permissions == null) {
-			List<Long> roleIds = Func.toLongList(roleId);
-			permissions = jdbcTemplate.queryForList(PermissionConstant.permissionAllStatement(roleIds.size()), roleIds.toArray(), String.class);
-			CacheUtil.put(SYS_CACHE, SCOPE_CACHE_CODE, roleId, permissions);
-		}
-		return permissions;
-	}
+//	private List<String> permissionPath(String roleId) {
+//		List<String> permissions = CacheUtil.get(SYS_CACHE, SCOPE_CACHE_CODE, roleId, List.class);
+//		if (permissions == null) {
+//			List<Long> roleIds = Func.toLongList(roleId);
+//			permissions = jdbcTemplate.queryForList(PermissionConstant.permissionAllStatement(roleIds.size()), roleIds.toArray(), String.class);
+//			CacheUtil.put(SYS_CACHE, SCOPE_CACHE_CODE, roleId, permissions);
+//		}
+//		return permissions;
+//	}
 
 	/**
 	 * 获取接口权限信息
@@ -79,16 +77,16 @@ public class PermissionHandler implements IPermissionHandler {
 	 * @param roleId     角色id
 	 * @return permissions
 	 */
-	private List<String> permissionCode(String permission, String roleId) {
-		List<String> permissions = CacheUtil.get(SYS_CACHE, SCOPE_CACHE_CODE, permission + StringPool.COLON + roleId, List.class);
-		if (permissions == null) {
-			List<Object> args = new ArrayList<>(Collections.singletonList(permission));
-			List<Long> roleIds = Func.toLongList(roleId);
-			args.addAll(roleIds);
-			permissions = jdbcTemplate.queryForList(PermissionConstant.permissionStatement(roleIds.size()), args.toArray(), String.class);
-			CacheUtil.put(SYS_CACHE, SCOPE_CACHE_CODE, permission + StringPool.COLON + roleId, permissions);
-		}
-		return permissions;
-	}
+//	private List<String> permissionCode(String permission, String roleId) {
+//		List<String> permissions = CacheUtil.get(SYS_CACHE, SCOPE_CACHE_CODE, permission + StringPool.COLON + roleId, List.class);
+//		if (permissions == null) {
+//			List<Object> args = new ArrayList<>(Collections.singletonList(permission));
+//			List<Long> roleIds = Func.toLongList(roleId);
+//			args.addAll(roleIds);
+//			permissions = jdbcTemplate.queryForList(PermissionConstant.permissionStatement(roleIds.size()), args.toArray(), String.class);
+//			CacheUtil.put(SYS_CACHE, SCOPE_CACHE_CODE, permission + StringPool.COLON + roleId, permissions);
+//		}
+//		return permissions;
+//	}
 
 }
